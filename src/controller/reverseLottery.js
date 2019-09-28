a78df1fc659b74eca03e75e52669c9d3a8522a09
fhfuih <sam.zyhuang@outlook.com>
2019-09-28 10:55:59 +0800

    lottery

---
+++ src/controller/reverseLottery.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const getInt = () => Math.round(Math.random() * 100 + 1);
+
+function reverseLottery(req, res) {
+  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  res.send(arr);
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const router = require('express-promise-router')();
 const sort = require('../controller/sort');
 const square = require('../controller/square');
 const genSeq = require('../controller/generateSequence');
+const lottery = require('../controller/reverseLottery');
 
 router.get('/hello', (req, res) => {
   const { name = 'world' } = req.query;
@@ -13,13 +14,8 @@ router.post('/sort', sort);
 
 router.post('/square', square);
 
-router.post(
-  '/generateSequence',
-  (req, res, next) => {
-    console.log(req.body);
-    next();
-  },
-  genSeq
-);
+router.post('/generateSequence', genSeq);
+
+router.post('/lottery', lottery);
 
 module.exports = router;
